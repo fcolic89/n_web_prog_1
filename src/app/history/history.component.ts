4386c4bfb455a90c4062fa8d5bcfab21176fbcfb
fcolic <fcolic8920rn@raf.rs>
2022-10-19 01:25:31 +0200

    history

---
+++ src/app/history/history.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {HistoryService} from "../services/history.service";
+
+@Component({
+  selector: 'app-history',
+  templateUrl: './history.component.html',
+  styleUrls: ['./history.component.css']
+})
+export class HistoryComponent implements OnInit {
+
+  history: string[] = [];
+
+  constructor(private route: ActivatedRoute, private historyService: HistoryService) { }
+
+  ngOnInit(): void {
+    this.history =  this.historyService.getHistory();
+  }
+
+
+}
